c18f0e93a0bdacea30d66b88a2863d07831bd9d9
Armaan Gupta <ohayouarmaan@gmail.com>
2024-03-17 11:59:11 +0530

    feat: home-page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,15 +4,11 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="relative mt-[90px] pb-4 flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
+        {/* //logo-font logo-animation font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-500 text-8xl animate-gradient bg-300% */}
+        <h1 className="logo-font p-5 logo-animation font-bold text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-pink-500 text-8xl animate-gradient bg-300%">
+          Bright Sense
+        </h1>
       </div>
 
       <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
@@ -22,14 +18,14 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
+          <h2 className={`mb-3 text-2xl logo-font font-semibold`}>
+            Fan{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
+          <p className={`m-0 max-w-[30ch] text-sm logo-font opacity-50`}>
+            Control the Fan using this.
           </p>
         </a>
 
@@ -39,14 +35,14 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
+          <h2 className={`mb-3 text-2xl logo-font`}>
+            Bulb{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          <p className={`m-0 max-w-[30ch] logo-font text-sm opacity-50`}>
+            Control the Fan using this.
           </p>
         </a>
 
@@ -56,14 +52,14 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
+          <h2 className={`mb-3 text-2xl logo-font font-semibold`}>
+            LED{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
+          <p className={`m-0 max-w-[30ch] logo-font text-sm opacity-50`}>
+            Control your home LED using this.
           </p>
         </a>
 
@@ -73,14 +69,14 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
+          <h2 className={`mb-3 text-2xl logo-font font-semibold`}>
+            Air Conditioner{" "}
             <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          <p className={`m-0 max-w-[30ch] logo-font text-sm opacity-50 text-balance`}>
+            Instantly change your AC status and temperature.
           </p>
         </a>
       </div>
